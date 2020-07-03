131c4a677d91ddf5bb9804fd275db20dc36786de
Kelton Gallant <kgallant90@gmail.com>
2020-07-03 19:20:20 -0400

    Preload gallery images

--- js/gallery.js
+++ js/gallery.js
@@ -5,12 +5,14 @@ function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage = document.querySelector("#gallery-photo img");
 
-
-
   thumbnails.forEach(function(thumbnail) {
+    // Preload large images.
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
     thumbnail.addEventListener("click", function() {
       // Set clicked image as main image.
-      let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
 
       // Change which image is current.
@@ -29,46 +31,32 @@ function activateGallery() {
   });
 }
 
-
-// let newTitle = thumbnail.dataset.title;
-// mainTitle.setAttribute(newTitle);
-//
-// let newDescription = thumbnail.dataset.description;
-// mainDescription.setAttribute(newDescription);
-
-
 // // Activates the image gallery.
 // // The main task is to attach an event listener to each image in the gallery
 // // and respond appropriately on click.
-//
 // function activateGallery() {
-//   let thumbnails = document.querySelector("#gallery-thumbs").
-//                             querySelectorAll("img");
+//   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
 //   let mainImage = document.querySelector("#gallery-photo img");
 //
 //   thumbnails.forEach(function(thumbnail) {
+//
 //     thumbnail.addEventListener("click", function() {
 //       // Set clicked image as main image.
 //       let newImageSrc = thumbnail.dataset.largeVersion;
 //       mainImage.setAttribute("src", newImageSrc);
-      //
-      //
-      // // Change which image is current.
-      // document.querySelector(".current").classList.remove("current");
-      // thumbnail.parentNode.classList.add("current");
-      //
-      // // // Set clicked image as gallery info
-      // // let newTitle = thumbnail.dataset.title;
-      // // mainTitle.setAttribute(nodeValue, newTitle);
+//
+//       // Change which image is current.
+//       document.querySelector(".current").classList.remove("current");
+//       thumbnail.parentNode.classList.add("current");
+//
+//       // Update image info.
+//       let galleryInfo = document.querySelector("#gallery-info");
+//       let title = galleryInfo.querySelector(".title");
+//       let description = galleryInfo.querySelector(".description");
+//
+//       title.innerHTML = thumbnail.dataset.title;
+//       description.innerHTML = thumbnail.dataset.description;
+//
 //     });
 //   });
-      // altDescription.forEach(function(descript) {
-      //   descript.addEventListener("click", function() {
-      //     // Set clicked image as info.
-      //     let newDescript = descript.dataset.description;
-      //     altDescription.setAttribute(newDescript);
-      //   });
-      // });
-      //
-      // altDescription.forEach(function(alt))
 // }
